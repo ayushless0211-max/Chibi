3947d5ca9d58ec703582227c98e6d0f4b82f84b8
ayushless0211-max <ayushless0211@gmail.com>
2026-05-19 22:08:42 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             try {
                 // STEP A: Sahi Tunnel Route par hit karna (`/api/create-payment-order`)
-                const orderRes = await fetch('https://e3ab27c32c92db.lhr.life/api/create-payment-order', {
+                const orderRes = await fetch('https://0ba04fd6523b65.lhr.life/api/create-payment-order', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ cartItems: cart })
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         placeOrderBtn.innerText = "Verifying Payment Integrity...";
                         
                         // STEP C: Verification data ke liye sahi endpoint route (`/api/verify-payment`)
-                        const verifyRes = await fetch('https://e3ab27c32c92db.lhr.life/api/verify-payment', {
+                        const verifyRes = await fetch('https://0ba04fd6523b65.lhr.life/api/verify-payment', {
                             method: 'POST',
                             headers: { 'Content-Type': 'application/json' },
                             body: JSON.stringify({
